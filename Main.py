f7d4dd15102da9dbbb899d4c3abbd1447289909e
Brandon <Brandon>
2016-11-21 23:51:02 -0500

    All Programming Is Done, Try It Out :D

--- Main.py
+++ Main.py
@@ -1,17 +1,24 @@
 import numpy as np
+from sklearn.naive_bayes import GaussianNB
 import re
-import string
 import codecs
 
+
 words = {}
+lines = []
+Vectors = [[]]
+Sentiments = []
+file = "Democrat.csv"
 
 # Populates The Dictionary From The Provided File
 def save_words():
-	print("Building Dictionary, This Might Take Awhile...");
-	with codecs.open('Trump.csv', "rb",encoding='utf-8', errors='ignore') as f:
+	print("Building Dictionary, This Might Take Awhile...")
+	with codecs.open(file, "rb",encoding='utf-8', errors='ignore') as f:
 		word = 0
 		for line in f:
-			wrds = line.replace('\\n','').replace('\n','').split(" ")
+			wrds = re.split(",|\s",line.replace('\\n','').replace('\n',''))
+			Sentiments.append(wrds.pop(0))
+			lines.append(wrds)
 			for wrd in wrds:
 				try:
 					if wrd not in words.keys():
@@ -20,17 +27,70 @@ def save_words():
 				except:
 					""
 		print("Done! Length: "+str(words.__len__()))
+	print("Writing File: Words.csv...")
 	file = open('Words.csv','w',encoding='utf-8', errors='ignore')
 	for ww in words.keys():
 		file.write(ww+"\n")
-
-
-# Returns The Index That The Passed In Word Is Located, Otherwise Returns -1 If Not Found
+	print("Writing File: Sentiments.csv...")
+	file = open('Sentiments.csv','w',encoding='utf-8', errors='ignore')
+	for s in Sentiments:
+		file.write(s)
+		file.write("\n")
+		
+		
+# Returns The Index Of The Passed In Word, Otherwise Returns -1 If Not Found
 def find_word_index(str):
 	if str in words.keys():
 		return words[str]
 	else:
 		return -1
-	
 
+
+# Builds The Vectors 		
+def save_vectors():
+	print("Building Vectors, This Might Take Awhile...")
+	for line in lines:
+		vec = [0]*words.__len__()
+		for wd in line:
+			vec[find_word_index(wd)] += 1
+		Vectors.append(vec)
+	Vectors.pop(0)
+	print("Done! Length: "+str(Vectors.__len__()))
+	print("Writing File: Vectors.csv...")
+	file = open('Vectors.csv','w',encoding='utf-8', errors='ignore')
+	for v in Vectors:
+		for vv in v:
+			file.write(str(vv)+",")
+		file.write("\n")
+		
+
+# Converts A String Into A Vector - Please Use Spaces Between The Words In Your Sentence
+def convert_to_vector(str):
+	s = str.split(" ")
+	ss = [0]*words.__len__()
+	for w in s:
+		ss[find_word_index(w)] += 1
+	return ss
+
+	
+# Predict How Many Votes, Up Or Down, A Comment May Receive
+def predict(str):
+	print("Comment: '"+str+"'")
+	print("I Think The Comment Will Get "+clf.predict([convert_to_vector(str)])[0]+" Votes")
+
+	
+def accept_input():
+	var = ""
+	while var != "Exit":
+		var = input("\nEnter A Comment For The Classifier To Score ( Exit To Quit )\n")
+		if var != "Exit":
+			predict(var)
+
+
+print("Getting Stuff Ready...")	
 save_words()
+save_vectors()
+print("Fitting Data...")
+clf = GaussianNB()
+clf.fit(Vectors, Sentiments)
+accept_input()
---
+++ readme.md
@@ -0,0 +1,18 @@
+CRITICAL IMPORTANCE:
+
+	WARNING: LOADING CERTAIN DATA FILES SUCH AS TRUMP.CVS
+	WILL LOCK UP YOUR COMPUTER IF YOU DON'T HAVE A TON OF
+	RAM!
+
+	USE WITH CAUTION OR MEMORY SWAPPING WILL OCCUR AND YOU
+	WILL MOST LIKELY NEED TO RESTART YOUR MACHINE!
+	
+
+HIGH IMPORTANCE:
+
+	YAY: The Program Works With The Democrat File. I Have Some
+	Pretty Reasonable Scoring Coming Back. You Type In All Test
+	Comments Directly Into The Console Instead Of Having To Reset
+	The Program Each Time You Want To Test Another Comment.
+	
+	Getting Stuff Done Like A Boss!
